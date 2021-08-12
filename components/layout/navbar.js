31e3026516e73ebcbabefdb0c4575bee9e97930d
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-08-12 18:23:20 +0530

    just a commit
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -3,19 +3,15 @@ import { auth } from "../../firebase/firebase";
 import Link from "next/link";
 import Alerts from "../Main/alerts";
 import { useState, useEffect } from "react";
-
 import navStyles from "../../styles/navStyles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Avatar, Badge, Menu, MenuItem } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
+import { Avatar, Menu, MenuItem } from "@material-ui/core";
 
 export default function Navbar({ user }) {
-  const classes = navStyles();
+  const navClass = navStyles();
 
   const [message, setMessage] = useState("");
   const [severity, setSeverity] = useState("");
@@ -39,32 +35,31 @@ export default function Navbar({ user }) {
   };
 
   return (
-    <div className={classes.nav}>
+    <div className={navClass.nav}>
       <AppBar position="static">
         <Toolbar>
           <Avatar
             alt="Kaalashiva"
             variant="rounded"
             src="https://image.shutterstock.com/image-photo/adiyogi-lord-shiva-statue-isha-260nw-1886897623.jpg"
-            className={classes.menuButton}
+            className={navClass.menuButton}
           />
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className={navClass.title}>
             <Link href="/">ಕಾಲಶಿವ</Link>
           </Typography>
           <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls="menu-userBar"
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <Avatar alt={user?.displayName} src={user?.photoURL} />
-              </IconButton>
+            edge="end"
+            aria-label="account of current user"
+            aria-controls="menu-userBar"
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+          >
+            <Avatar alt={user?.displayName} src={user?.photoURL} />
+          </IconButton>
 
           {user ? (
             <>
-              
               <Menu
                 anchorEl={anchorEl}
                 anchorOrigin={{ vertical: "top", horizontal: "right" }}
@@ -75,27 +70,29 @@ export default function Navbar({ user }) {
                 onClose={handleMenuClose}
               >
                 <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-                <MenuItem onClick={() => {
-                  auth
-                    .signOut()
-                    .then(() => {
-                      setMessage("Log-out Successful");
-                      setSeverity("success");
-                      setShowMessage(true);
-                    })
-                    .catch((error) => {
-                      setMessage(`${error.message} - ${error.code}`);
-                      setSeverity("error");
-                      setShowMessage(true);
-                    });
-                }}>Logout</MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    auth
+                      .signOut()
+                      .then(() => {
+                        setMessage("Log-out Successful");
+                        setSeverity("success");
+                        setShowMessage(true);
+                      })
+                      .catch((error) => {
+                        setMessage(`${error.message} - ${error.code}`);
+                        setSeverity("error");
+                        setShowMessage(true);
+                      });
+                  }}
+                >
+                  Logout
+                </MenuItem>
               </Menu>
-              
-                
             </>
           ) : (
             <>
-            <Menu
+              <Menu
                 anchorEl={anchorEl}
                 anchorOrigin={{ vertical: "top", horizontal: "right" }}
                 id="menu-userBar"
@@ -104,10 +101,13 @@ export default function Navbar({ user }) {
                 open={isMenuOpen}
                 onClose={handleMenuClose}
               >
-                <MenuItem onClick={handleMenuClose}><Link href="/login">Login</Link></MenuItem>
-                <MenuItem onClick={handleMenuClose}><Link href="/signup">Signup</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link href="/login">Login</Link>
+                </MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link href="/signup">Signup</Link>
+                </MenuItem>
               </Menu>
-             
             </>
           )}
         </Toolbar>
