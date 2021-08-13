7b2c6e032c97fe91848529363243cd32313a56f8
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-08-13 16:33:46 +0530

    Added tab icon
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

--- components/Main/tabs.js
+++ components/Main/tabs.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
-import { Tab, Tabs, AppBar } from "@material-ui/core";
+import { Tab, Tabs, AppBar, Grid } from "@material-ui/core";
+import { BrandingWatermark, Home } from "@material-ui/icons";
 
 export default function HomeTabs() {
   const categories = ["ಲೇಖನ", "ಕಥೆ", "ಕವಿತೆ"];
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(-1);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -13,7 +14,16 @@ export default function HomeTabs() {
   return (
     <div>
       <AppBar position="static" color="default" style={{margin:"1rem 0"}}>
-        <Tabs
+        <Grid container>
+          <Grid item >
+          <Link href="/">
+            <a>
+              <Tab label="Home" icon={<Home/>} />
+            </a>
+          </Link>
+          </Grid>
+          <Grid item  >
+          <Tabs
           value={value}
           onChange={handleChange}
           indicatorColor="primary"
@@ -22,20 +32,16 @@ export default function HomeTabs() {
           scrollButtons="auto"
           aria-label="scrollable auto tabs example"
         >
-          <Link href="/">
-            <a>
-              <Tab label="Home" />
-            </a>
-          </Link>
-
           {categories.map((category) => (
             <Link href={`/${category}`} key={category}>
               <a>
-                <Tab label={category} />
+                <Tab label={category} icon={<BrandingWatermark style={{ fontSize: 20 }}/>} />
               </a>
             </Link>
           ))}
         </Tabs>
+        </Grid>
+        </Grid>
       </AppBar>
     </div>
   );
